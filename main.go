86573fd20067c3ccd6d0cef94046c3aded7836b4
Cristian Lopez <c88lopez@gmail.com>
2016-12-14 13:34:47 -0300

    saraza

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "github.com/c88lopez/dbs/command"
 /**
 This is only the entry point of the application.
 The idea is to handle all the input on a separated component.
- */
+*/
 func main() {
 	if err := command.Execute(); nil != err {
 		panic(err)
