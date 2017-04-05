3ee5d9a741758bcb847e5efc8159ce659ad3e35b
clopez <c88lopez@gmail.com>
2017-04-05 19:56:55 -0300

    asd

--- src/command/dbs.go
+++ src/command/dbs.go
@@ -3,13 +3,19 @@ package command
 import (
 	"fmt"
 	"time"
+
+	"github.com/c88lopez/dbs/src/handlers"
 )
 
 // Executes the dbs.
 func Execute() error {
+	var err error
+
 	start := time.Now()
 
-	Dispatch()
+	if err = Dispatch(); nil != err {
+		handlers.Error(err)
+	}
 
 	fmt.Printf("\nElapsed time: %s\n", time.Since(start))
 	return nil
