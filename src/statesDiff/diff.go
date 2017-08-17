6f862a7b14baf0ac4e37f524d02cf1b9c55bf9c9
clopez <c88lopez@gmail.com>
2017-08-17 16:32:43 -0300

    asd

--- src/statesDiff/diff.go
+++ src/statesDiff/diff.go
@@ -1,6 +1,14 @@
 package statesDiff
 
-import "os"
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/c88lopez/dbs/src/entity"
+)
 
 // func Diff
 func Diff() error {
@@ -15,6 +23,85 @@ func Diff() error {
 }
 
 func diffCurrentPrevious() error {
+	dir, err := os.Getwd()
+	if nil != err {
+		return err
+	}
+
+	/**
+	get paths (we can put these as get in another place
+	*/
+	statesDir := dir + "/.dbs/states"
+	historyDir := statesDir + "/history"
+	currentLink := statesDir + "/current"
+
+	fmt.Printf("%s\n", currentLink)
+
+	currentState, _ := os.Readlink(currentLink)
+
+	fmt.Printf("%v\n", currentState)
+
+	f, err := os.Open(historyDir)
+	if err != nil {
+		return err
+	}
+
+	/**
+	searching the following state (we need to handle if there is no next state)
+	*/
+	var nextState string
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		if strings.Contains(scanner.Text(), currentState) {
+			scanner.Scan()
+			nextState = scanner.Text()
+
+			break
+		}
+	}
+
+	/**
+	decoding both current and next states
+	*/
+	file, err := os.Open(statesDir + "/" + currentState)
+	if nil != err {
+		return err
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+
+	currentSchema := new(entity.Schema)
+
+	err = decoder.Decode(currentSchema)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("%v\n", currentSchema.Tables)
+
+	fmt.Printf("nextState: %s\n", nextState)
+
+	file, err = os.Open(statesDir + "/" + nextState)
+	if nil != err {
+		return err
+	}
+	defer file.Close()
+
+	decoder = json.NewDecoder(file)
+
+	nextSchema := new(entity.Schema)
+
+	err = decoder.Decode(nextSchema)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("%v\n", nextSchema.Tables)
+
+	/**
+	here we check deeply for differences
+	*/
 
 	return nil
 }
