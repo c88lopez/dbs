be771767d5aa2b1f8df5983ae565dd7111f3e18e
clopez <c88lopez@gmail.com>
2017-08-25 18:12:40 -0300

    asd

--- src/statesDiff/diff.go
+++ src/statesDiff/diff.go
@@ -35,11 +35,11 @@ func diffCurrentPrevious() error {
 	historyDir := statesDir + "/history"
 	currentLink := statesDir + "/current"
 
-	fmt.Printf("%s\n", currentLink)
+	fmt.Printf("currentLink: %s\n", currentLink)
 
 	currentState, _ := os.Readlink(currentLink)
 
-	fmt.Printf("%v\n", currentState)
+	fmt.Printf("currentState: %v\n", currentState)
 
 	f, err := os.Open(historyDir)
 	if err != nil {
@@ -78,7 +78,7 @@ func diffCurrentPrevious() error {
 		return err
 	}
 
-	fmt.Printf("%v\n", currentSchema.Tables)
+	fmt.Printf("currentSchema.Tables: %v\n", currentSchema.Tables)
 
 	fmt.Printf("nextState: %s\n", nextState)
 
@@ -97,11 +97,64 @@ func diffCurrentPrevious() error {
 		return err
 	}
 
-	fmt.Printf("%v\n", nextSchema.Tables)
+	fmt.Printf("nextSchema.Tables:  %v\n", nextSchema.Tables)
 
 	/**
 	here we check deeply for differences
 	*/
+	var checkingTableIndex int
+	var checkingColumnIndex int
+	for _, csTable := range currentSchema.Tables {
+		checkingTableIndex = -1
+
+		fmt.Printf("table: %s\n", csTable.Name)
+
+		/**
+		Looking if the table exists
+		*/
+		for nsTableIndex, nsTable := range nextSchema.Tables {
+			if nsTable.Name == csTable.Name {
+				checkingTableIndex = nsTableIndex
+				break
+			}
+		}
+
+		if checkingTableIndex == -1 {
+			// Table does not exists => drop
+			fmt.Printf("Should drop table %s", csTable.Name)
+		} else {
+			// Table found, checking columns
+			for _, csColumn := range csTable.Columns {
+				fmt.Printf("column: %s\n", csColumn.Name)
+
+				/**
+				Looking if the column exists
+				*/
+				checkingColumnIndex = -1
+				for nsColumnIndex, nsColumn := range nextSchema.Tables[checkingTableIndex].Columns {
+					if nsColumn.Name == csColumn.Name {
+						checkingColumnIndex = nsColumnIndex
+
+						if csColumn.DataType != nsColumn.DataType ||
+							csColumn.Nullable != nsColumn.Nullable ||
+							csColumn.Key != nsColumn.Key ||
+							csColumn.DefaultValue != nsColumn.DefaultValue ||
+							csColumn.Extra != nsColumn.Extra {
+							// The column has not the same definition
+							fmt.Printf("Should alter column %s.%s\n", csTable.Name, csColumn.Name)
+						}
+
+						break
+					}
+				}
+
+				if checkingColumnIndex == -1 {
+					// Column does not exists => drop
+					fmt.Printf("Should drop column %s.%s", csTable.Name, csColumn.Name)
+				}
+			}
+		}
+	}
 
 	return nil
 }
